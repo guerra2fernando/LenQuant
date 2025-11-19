d62f43e90f8be93b8626aa3e5dc0585ee7c1a2ee
guerra2fernando <fern2gue@gmail.com>
2025-11-19 16:48:39 +0400

    fix logo image and docker configuration

--- web/next-app/next.config.js
+++ web/next-app/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
     unoptimized: true, // Disable image optimization to avoid build issues
     domains: ['lenquant.com', 'localhost'],
