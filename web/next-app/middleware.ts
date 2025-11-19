2fa351bb246812901a2bf36f8683ca3506752240
guerra2fernando <fern2gue@gmail.com>
2025-11-19 17:02:28 +0400

    add images folder to middleware

--- web/next-app/middleware.ts
+++ web/next-app/middleware.ts
@@ -11,11 +11,13 @@ export const config = {
     /*
      * Match all request paths except:
      * - /login (login page)
+     * - /terms, /privacy (public pages)
      * - /api/auth/* (NextAuth endpoints)
      * - /_next/* (Next.js internals)
+     * - /images/* (static images)
      * - /favicon.ico, /robots.txt, etc. (static files)
      */
-    "/((?!login|api/auth|_next|favicon.ico|robots.txt).*)",
+    "/((?!login|terms|privacy|api/auth|_next|images|favicon.ico|robots.txt).*)",
   ],
 };
 
